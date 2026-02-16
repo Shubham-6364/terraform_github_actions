e78fafd220d7dc35456750f1c02eaa0473cd75ba
root <root@DESKTOP-P4JG11D.localdomain>
2026-02-16 17:10:57 +0000

    first commit

---
+++ main.tf
@@ -0,0 +1,13 @@
+resource "aws_s3_bucket" "demo_bucket" {
+  bucket = "my-terraform-demo-bucket-123456"
+}
+
+resource "aws_instance" "demo_instance" {
+  ami           = "ami-073130f74f5ffb161"  # Ubuntu Linux (eu-north-1)
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "GitHubActions-EC2"
+  }
+}
+
---
+++ output.tf
@@ -0,0 +1,4 @@
+output "instance_public_ip" {
+  value = aws_instance.demo_instance.public_ip
+}
+
