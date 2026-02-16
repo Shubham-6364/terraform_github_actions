46feb0680d1a8242b207f9492a0792bd835babae
Shubham-6364 <play.video2525@gmail.com>
2026-02-16 23:10:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,3 @@
-resource "aws_s3_bucket" "demo_bucket" {
-  bucket = "shubham1.codersdiary.shop"
-}
-
 resource "aws_instance" "demo_instance" {
   ami           = "ami-073130f74f5ffb161" # Ubuntu Linux (eu-north-1)
   instance_type = var.instance_type
