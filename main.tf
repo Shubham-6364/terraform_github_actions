f92945d6c56e23df3f05ee2703fc59867952eb78
root <root@DESKTOP-P4JG11D.localdomain>
2026-02-16 17:28:10 +0000

    format fixed

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "aws_s3_bucket" "demo_bucket" {
-  bucket = "my-terraform-demo-bucket-123456"
+  bucket = "shubham1.codersdiary.shop"
 }
 
 resource "aws_instance" "demo_instance" {
-  ami           = "ami-073130f74f5ffb161"  # Ubuntu Linux (eu-north-1)
+  ami           = "ami-073130f74f5ffb161" # Ubuntu Linux (eu-north-1)
   instance_type = var.instance_type
 
   tags = {
